c1be1de4096100f2c59d1b0b5d84742ec8300c52
Pedro <rodriguesdesouza.pedro@gmail.com>
2015-10-18 18:37:08 -0200

    coloquei front no clinicas

--- application/views/insere_clinicas.php
+++ application/views/insere_clinicas.php
@@ -1,78 +1,69 @@
-<html>
-    <head></head>
-    <body>
-        <section class="formCadastroClinicas">
-            <form method="post" action="<?php echo base_url() ?>clinica/insereClinica/">
-                <div class="itemFormCadastroClinicas">
-                    <label>Nome</label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="nomeClinica" />
+<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/forms.css" />
+<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/medico/medico.css" />
+<section class="mainContent">
+
+
+
+        <section class="cadastro_medico">
+            <div class="containerCenter fullSiteSize">
+                <form method="post" action="<?php echo base_url() ?>clinica/insereClinica/">
+                    <div class="halfSize">
+                        <h2>Cadastrar Clinica</h2>
+                        <div class="container_item_form">
+                            <label>Nome</label>
+                            <input type="text" name="nomeClinica" />            
+                        </div>
+                        <div class="container_item_form">
+                            <label>Telefone</label>
+                            <input type="text" name="telefoneClinica" />
+                        </div>
+                        <div class="container_item_form">
+                            <label>Endereço</label>
+                            <input type="text" name="enderecoClinica" />
+                        </div>
+                        <div class="container_item_form">
+                            <label>Estado</label>
+                            <select name="ufClinica">
+                                <option >Selecione</option>
+                                <?php
+                                foreach ($estados as $estado):
+                                    ?>
+                                    <option value="<?= $estado->uf ?>"><?= $estado->uf ?></option>
+                                    <?php
+                                endforeach;
+                                ?>
+                            </select>
+                        </div>	
+                        <div class="container_item_form">
+                            <label>Numero</label>
+                            <input type="text" name="numeroClinica" />
+                        </div>					
+
+                        <div class="container_item_form">
+                            <label>Complemento</label>
+                            <input type="text" name="complementoClinica" />
+                        </div>
+                        <div class="container_item_form">
+                            <label>Bairro</label>
+                            <input type="text" name="bairroClinica" />
+                        </div>
+                        <div class="container_item_form">
+                            <label>Cidade</label>
+                            <input type="text" name="cidadeClinica" />
+                        </div>
+                        <div class="container_item_form">
+                            <label>Cep:</label>
+                            <input type="text" name="cepClinica" />
+                        </div>
+                        <div class="container_submit">
+                            <input type="submit" class="input_submit" />
+                            <button type="button" class="input_cancel">Cancelar</button>
+                        </div>	
                     </div>
-                </div>
-                <div class="itemFormCadastroClinicas">
-                    <label>Telefone</label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="telefoneClinica" />
-                    </div>
-                </div>
-                <div class="itemFormCadastroClinicas">
-                    <label>Endereço/label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="enderecoClinica" />
-                    </div>
-                </div>
-                <div class="itemFormCadastroClinicas">
-                    <label>Estado</label>
-                    <div class="campoCadastroClinica">
-                        <select name="ufClinica">
-                            <option >Selecione</option>
-                            <?php 
-                                foreach($estados as $estado):
-                            ?>
-                            <option value="<?=$estado->uf ?>"><?=$estado->uf ?></option>
-                            <?php
-                            endforeach;
-                            ?>
-                        </select>
-                    </div>
-                </div>	
-                <div class="itemFormCadastroClinicas">
-                    <label>Numero</label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="numeroClinica" />
-                    </div>
-                </div>					
-        	
-                <div class="itemFormCadastroClinicas">
-                    <label>Complemento</label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="complementoClinica" />
-                    </div>
-                </div>
-                <div class="itemFormCadastroClinicas">
-                    <label>Bairro</label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="bairroClinica" />
-                    </div>
-                </div>
-                <div class="itemFormCadastroClinicas">
-                    <label>Cidade</label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="cidadeClinica" />
-                    </div>
-                </div>
-                <div class="itemFormCadastroClinicas">
-                    <label>Cep:</label>
-                    <div class="campoCadastroClinica">
-                        <input type="text" name="cepClinica" />
-                    </div>
-                </div>
-                <div class="itemFormCadastroClinicas">
-                    <div class="campoCadastroClinica">
-                        <input type="submit"  value="confirmar" />
-                    </div>
-                </div>	
-            </form>
+                    </form>
+            </div>
+            
         </section>
-    </body>
-</html>
+    
+
+</section>
