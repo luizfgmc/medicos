b741c69dc9d8ccc6174490fcd8408d46f638a029
Pedro <rodriguesdesouza.pedro@gmail.com>
2015-10-24 15:46:33 -0200

    terminei a home

---
+++ application/views/home.php
@@ -0,0 +1,46 @@
+
+<section class="bemVindo">
+    <div class="containerCenter fullSiteSize">
+        <h2>BEM VINDO AO AMIGO MÉDICO</h2>
+    </div>
+</section>
+
+<section class="medicoInstituicao">
+    <div class="containerCenter fullSiteSize">
+        <div class="conteudoMedicoInstituicao">
+            <div class="imagemMedicoInstituicao">
+                <img src="<?php echo base_url() ?>assets/images/medicoHome.jpg" />
+            </div>
+            <div class="escolhaMedicoInsituicao">
+                <h3> Você é medico ou instituição ?</h3>
+                <div class="containerMedico">
+                    <a href=""><div class="trianguloEsquerda"></div></a>
+                    <a href=""> Médico </a>
+                </div>
+                <div class="containerInstituicao">
+                    <a href="">  Instituicao</a>
+                    <a href=""><div class="trianguloDireita"></div></a>
+                </div>
+            </div>
+            <div class="imagemMedicoInstituicao">
+                <img src="<?php echo base_url() ?>assets/images/instituicaoHome.jpg" />
+            </div>
+        </div>
+    </div>
+</section>
+<section class="estatistcasHome">
+    <div class="containerCenter fullSiteSize">
+        <h3> NO MÊS DE OUTUBRO </h3>
+        <div class="estatisticas">
+            <div class="itemEstatisticas">
+                165 consultas
+            </div>
+            <div class="itemEstatisticas">
+                37 médicos
+            </div>
+            <div class="itemEstatisticas">
+                5 instituições
+            </div>
+        </div>
+    </div>
+</section>
