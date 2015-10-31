3a5cb2c9f53ad3aa63eadae2da952c35eb2bd208
Pedro <rodriguesdesouza.pedro@gmail.com>
2015-10-31 18:34:29 -0200

    alguns detalhes da lista

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,55 +1,53 @@
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<link href='https://fonts.googleapis.com/css?family=Actor' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/main.css" />
-                <script src="<?php echo base_url() ?>assets/js/jquery1.9.js"></script>
-                <script src="<?php echo base_url() ?>assets/js/validate.js"></script>
-                <script src="<?php echo base_url() ?>assets/js/main.js"></script>
-	</head>
-<header class="mainHeader">
+    <head>
+        <meta charset="utf-8" />
+        <link href='https://fonts.googleapis.com/css?family=Actor' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/main.css" />
+        <script src="<?php echo base_url() ?>assets/js/jquery1.9.js"></script>
+        <script src="<?php echo base_url() ?>assets/js/validate.js"></script>
+        <script src="<?php echo base_url() ?>assets/js/main.js"></script>
+    </head>
+    <header class="mainHeader">
         <div class="containerCenter fullSiteSize">
-                <section class="containerItemsHeader">
-                        <div class="iconMenuTop">
-                                        <a href="">
-                                                <img src="<?php echo base_url()?>assets/images/menu_topo.png" />
-                                        </a>	
-                            <div class="menuTop">
-                                <ul>
-                                    <li><a href="<?php echo base_url() ?>medico">Página Principal</a><li>
-                                    <li><a href="<?php echo base_url() ?>medico/solicitacoes/">Solicitações</a><li>
-                                    <li><a href="<?php echo base_url() ?>agenda">Agenda</a><li>
-                                    <li><a href="<?php echo base_url() ?>clinica">Clínicas</a><li>
-                                </ul>    
-                            </div>
-                        </div>
-                        <div class="alertTop">
-                                        <a href="">
-                                                <img src="<?php echo base_url() ?>assets/images/alerta_topo.png" />
-                                        </a>
-                                     <div class="reminderTop">  
-                                    </div>
-                        </div>
-                        <div class="logoMedicoAmigoTop">
-                                        <a href="">
-                                                <img src="<?php echo base_url() ?>assets/images/logo_topo.png" />
-                                        </a>	
-                                        <span> | amigo médico</span>
-
-                        </div>
-                        <div class="medicoLoged">
-                                        <a href="">
-                                                <img src="<?php echo base_url() ?>assets/images/foto_medico_topo.png" />
-                                        </a>	
-                                        <span> Dr. Fabio Capelo</span>
-                        </div>
-                        <div class="configTop">
-                                        <a href="">
-                                                <img src="<?php echo base_url() ?>assets/images/conf_topo.png" />
-                                        </a>
-                             <div class="menuConfigTop">  
-                            </div>
-                        </div>
+            <section class="containerItemsHeader">
+                <div class="iconMenuTop">
+                    <a href="">
+                        <img src="<?php echo base_url() ?>assets/images/menu_topo.png" />
+                    </a>	
+                    <div class="menuTop">
+                        <ul>
+                            <li><a href="<?php echo base_url() ?>medico">Página Principal</a><li>
+                            <li><a href="<?php echo base_url() ?>medico/solicitacoes/">Solicitações</a><li>
+                            <li><a href="<?php echo base_url() ?>agenda">Agenda</a><li>
+                            <li><a href="<?php echo base_url() ?>clinica">Clínicas</a><li>
+                        </ul>    
+                    </div>
                 </div>
-        </section>
+                <div class="alertTop">
+                    <a href="">
+                        <img src="<?php echo base_url() ?>assets/images/alerta_topo.png" />
+                    </a>
+                    <div class="reminderTop">  
+                    </div>
+                </div>
+                <div class="logoMedicoAmigoTop">
+                    <a href="">
+                        <img src="<?php echo base_url() ?>assets/images/logo_topo.png" />
+                    </a>	
+                    <span> | amigo médico</span>
+
+                </div>
+                <div class="medicoLoged">
+
+                    <span> <a href="<?php echo base_url() ?>medico/visualizaEditaMedicoMedicos/" >Dr. Fabio Capelo </a> </span>
+                </div>
+               <!-- <div class="configTop">
+                    <a href="">
+                        <img src="<?php echo base_url() ?>assets/images/conf_topo.png" />
+                    </a>
+                    <div class="menuConfigTop">  
+                    </div>
+                </div> -->
+        </div>
+    </section>
 </header>
