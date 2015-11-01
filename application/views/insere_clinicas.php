ea624a8f8e5c1d0fa018f5b178c9e235ad48f3d2
Luiz Flávio Gomes <luizflavio@outlook.com.br>
2015-11-01 18:18:23 -0200

    Validações Insere Clinicas
    Validações Insere Clinicas

--- application/views/insere_clinicas.php
+++ application/views/insere_clinicas.php
@@ -2,68 +2,65 @@
 <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/medico/medico.css" />
 <?php echo validation_errors(); ?>
 <section class="mainContent">
-       <section class="cadastro_medico">
-            <div class="containerCenter fullSiteSize">
-                <form method="post" action="<?php echo base_url() ?>clinica/insereClinica/" id="formClinica">
-                    <div class="halfSize">
-                        <h2>Cadastrar Clinica</h2>
-                            <div class="container_item_form">
-                                <label>Nome</label>
-                                <input type="text" class="default_text_input required" name="nomeClinica" size=45 maxlength=45/>            
-                            </div>
-                            <div class="container_item_form">
-                                <label>Telefone</label>
-                                <input type="text" class="default_text_input required" name="telefoneClinica" size=12 maxlength=12 />
-                            </div>
-                             <div class="container_item_form">
-                                <label>Cep:</label>
-                                <input type="text" class="default_text_input required" name="cepClinica" id="cepClinica" size=08 maxlength=08 />
-                            </div>
-                            <div class="container_item_form">
-                                <label>Endereço</label>
-                                <input type="text" class="default_text_input required" name="enderecoClinica" id="endereco" size=60 maxlength=60/>
-                            </div>
-                            <div class="container_item_form">
-                                <label>Numero</label>
-                                <input type="text" class="default_text_input required" name="numeroClinica" id = "numero" size=06 maxlength=06/>
-                            </div>					
+ <section class="cadastro_medico">
+    <div class="containerCenter fullSiteSize">
+        <form method="post" action="<?php echo base_url() ?>clinica/insereClinica/" id="formClinica">
+            <div class="halfSize">
+                <h2>Cadastrar Clinica</h2>
+                <div class="container_item_form">
+                    <label>Nome</label>
+                    <input type="text" class="default_text_input required" name="nomeClinica" size=45 maxlength=45/>            
+                </div>
+                <div class="container_item_form">
+                    <label>Telefone</label>
+                    <input type="text" class="default_text_input required" name="telefoneClinica" size=12 maxlength=12 OnKeyPress="formatar('##-####-####', this)"/>
+                </div>
+                <div class="container_item_form">
+                    <label>Cep:</label>
+                    <input type="text" class="default_text_input required" name="cepClinica" id="cepClinica" size=08 maxlength=10 OnKeyPress="formatar('##.###-###', this)"/>
+                </div>
+                <div class="container_item_form">
+                    <label>Endereço</label>
+                    <input type="text" class="default_text_input required" name="enderecoClinica" id="endereco" size=60 maxlength=60/>
+                </div>
+                <div class="container_item_form">
+                    <label>Numero</label>
+                    <input type="text" class="default_text_input required" name="numeroClinica" id = "numero" size=06 maxlength=06/>
+                </div>                  
 
-                            <div class="container_item_form">
-                                <label>Complemento</label>
-                                <input type="text"  name="complementoClinica" id="complemento" size=15 maxlength=15/>
-                            </div>
-                            <div class="container_item_form">
-                                <label>Bairro</label>
-                                <input type="text" class="default_text_input required" name="bairroClinica" id="bairro" size=45 maxlength=45/>
-                            </div>
-                            <div class="container_item_form">
-                                <label>Cidade</label>
-                                <input type="text" class="default_text_input required" name="cidadeClinica" id="cidade" size=45 maxlength=45 />
-                            </div>
-                            <div class="container_item_form">
-                                <label>Estado</label>
-                                <select name="ufClinica">
-                                    <option >Selecione</option>
-                                    <?php
-                                    foreach ($estados as $estado):
-                                        ?>
-                                        <option value="<?= $estado->uf ?>"><?= $estado->uf ?></option>
-                                        <?php
-                                    endforeach;
-                                    ?>
-                                </select>
-                            </div>  
-                            <div class="container_submit">
-                                <input type="submit" class="input_submit" />
-                                <button type="button" class="input_cancel">Cancelar</button>
-                            </div>	
-                    </div>
-                </form>
+                <div class="container_item_form">
+                    <label>Complemento</label>
+                    <input type="text"  name="complementoClinica" id="complemento" size=15 maxlength=15/>
+                </div>
+                <div class="container_item_form">
+                    <label>Bairro</label>
+                    <input type="text" class="default_text_input required" name="bairroClinica" id="bairro" size=45 maxlength=45/>
+                </div>
+                <div class="container_item_form">
+                    <label>Cidade</label>
+                    <input type="text" class="default_text_input required" name="cidadeClinica" id="cidade" size=45 maxlength=45 />
+                </div>
+                <div class="container_item_form">
+                    <label>Estado</label>
+                    <select name="ufClinica">
+                        <option >Selecione</option>
+                        <?php
+                        foreach ($estados as $estado):
+                            ?>
+                        <option value="<?= $estado->uf ?>"><?= $estado->uf ?></option>
+                        <?php
+                        endforeach;
+                        ?>
+                    </select>
+                </div>  
+                <div class="container_submit">
+                    <input type="submit" class="input_submit" />
+                    <button type="button" class="input_cancel">Cancelar</button>
+                </div>  
             </div>
-            
-        </section>
-    
-
+        </form>
+    </div>
+</section>
 </section>
 
 <script>
