2b7e7159224b2184f1e9e5abd6ec876edde024d2
Luiz Flávio Gomes <luizflavio@outlook.com.br>
2015-11-29 23:43:19 -0200

    Cadastro de agendas - Retirado saldo do front
    Ajuste do front do cadastro de agendas.

--- application/views/insere_agenda.php
+++ application/views/insere_agenda.php
@@ -35,14 +35,8 @@
                 </section>
                 <section class="horariosOferecidos">
                     <p class="titleHorarioOfeceridos">Horários oferecidos:</p>
-
                     <form action="<?php echo base_url(); ?>agenda/insereAgenda" method="post">
-
-
                         <div class="campoCadastroAgenda">
-
-
-
                             <div class="containerQuantidadeHorario">
                                 <div class="container_item_form">
                                     <div class="container_tiny_item">
@@ -52,71 +46,66 @@
                                             <?php
                                             foreach ($clinica as $c):
                                                 ?>
-                                                <option value="<?= $c->id ?>"><?= $c->nome ?></option>
-                                                <?php
+                                            <option value="<?= $c->id ?>"><?= $c->nome ?></option>
+                                            <?php
                                             endforeach;
                                             ?>
                                         </select>
                                     </div>
-
-
                                     <div class="container_tiny_item">
                                         <label>Quantidade: </label>
                                         <input type="text" name="quantidadeAgenda" />
                                     </div>	
-
+<!--
                                     <div class="container_tiny_item">
                                         <label>Saldo: </label>
                                         <input type="text" name="saldoAgenda" />
                                     </div>	
-
-
-                                </div>
-                            </div> 
-                        </div>
-
-                            <div class="containerItemsDias">
-                                <div class="itemFormHorario">
-                                    <input type="radio" name="dia_agenda" value="Segunda-Feira" />
-                                    <label>Segunda-Feira</label>
-                                </div>	
-                                <div class="itemFormHorario">
-                                    <input type="radio" name="dia_agenda" value="Terça-Feira" />
-                                    <label>Terça-Feira</label>
-                                </div>
-                                <div class="itemFormHorario">
-                                    <input type="radio" name="dia_agenda" value="Quarta-Feira" />
-                                    <label>Quarta-Feira</label>
-                                </div>		
-                                <div class="itemFormHorario">
-                                    <input type="radio" name="dia_agenda" value="Quinta-Feira" />
-                                    <label>Quinta-Feira</label>
-                                </div>		
-                                <div class="itemFormHorario">
-                                    <input type="radio" name="dia_agenda" value="Sexta-Feira" />
-                                    <label>Sexta-Feira</label>
-                                </div>
-                                <div class="itemFormHorario">
-                                    <input type="radio" name="dia_agenda" value="Sábado" />
-                                    <label>Sábado</label>
-                                </div>	
-                                <div class="itemFormHorario">
-                                    <input type="radio" name="dia_agenda" value="Domingo" />
-                                    <label>Domingo</label>
-                                </div>
-                            </div>		
-
-
-                            <div class="form_horarios">
-                                <div class="itemFormHorario">
-                                    <div class="container_submit">
-                                        <input type="submit" class="input_submit" />
-                                        <a href="<?php echo base_url() ?>medico/solicitacoes/" class="input_cancel">Cancelar</a>
-                                    </div>
-
-                                </div>	
+-->
                             </div>
-                    </form>       
+                        </div> 
+                    </div>
+
+                    <div class="containerItemsDias">
+                        <div class="itemFormHorario">
+                            <input type="radio" name="dia_agenda" value="Segunda-Feira" />
+                            <label>Segunda-Feira</label>
+                        </div>	
+                        <div class="itemFormHorario">
+                            <input type="radio" name="dia_agenda" value="Terça-Feira" />
+                            <label>Terça-Feira</label>
+                        </div>
+                        <div class="itemFormHorario">
+                            <input type="radio" name="dia_agenda" value="Quarta-Feira" />
+                            <label>Quarta-Feira</label>
+                        </div>		
+                        <div class="itemFormHorario">
+                            <input type="radio" name="dia_agenda" value="Quinta-Feira" />
+                            <label>Quinta-Feira</label>
+                        </div>		
+                        <div class="itemFormHorario">
+                            <input type="radio" name="dia_agenda" value="Sexta-Feira" />
+                            <label>Sexta-Feira</label>
+                        </div>
+                        <div class="itemFormHorario">
+                            <input type="radio" name="dia_agenda" value="Sábado" />
+                            <label>Sábado</label>
+                        </div>	
+                        <div class="itemFormHorario">
+                            <input type="radio" name="dia_agenda" value="Domingo" />
+                            <label>Domingo</label>
+                        </div>
+                    </div>		
+                    <div class="form_horarios">
+                        <div class="itemFormHorario">
+                            <div class="container_submit">
+                                <input type="submit" class="input_submit" />
+                                <a href="<?php echo base_url() ?>medico/solicitacoes/" class="input_cancel">Cancelar</a>
+                            </div>
+
+                        </div>	
+                    </div>
+                </form>       
 
             </div>	
         </div>
