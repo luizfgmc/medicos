a37bc5ab8ab3bd2861d1e1022d1c16b742835bc4
Pedro <rodriguesdesouza.pedro@gmail.com>
2016-05-14 15:20:38 -0300

    Melhorei o layout do feedback do medico

---
+++ application/views/comentarioConsulta.php
@@ -0,0 +1,4 @@
+<?php
+<a href="#" id="enviarObservacao" valor="23" value="23"> comentário da consulta </a><br><textarea id="obs" rows="5" style="width:100%;"></textarea><button id="enviarObs" value="23"> Enviar </button><button id="cancelar" value="23"> Cancelar </button>  <br><br>  <br>
+                                           <input type="hidden" id="idSolicitacao" value="23">
+                                                                           </span>
--- assets/js/observacaoConsulta.js
+++ assets/js/observacaoConsulta.js
@@ -1,28 +1,30 @@
 
 	
-	var url = "http://localhost/medicos/"	
-	
+	var url = "http://localhost/medicos/"
+
+	// Linha da Solicitacao que vai ser apagada
+	var avo="";
 	$(document).on("click","#enviarObservacao", function(event) {
 
 		var id = $(this).attr('valor');
 		$('#obs').remove();
 		$('#cancelar').remove();
 		$('#enviarObs').remove();
-		$(this).after("<br/><textarea id='obs' rows='5' style=width:100%;></textarea><br/>");
-		$('#obs').after("<button id='enviarObs' value="+id+"> Enviar </button> ");
-		$('#enviarObs').after("<button id='cancelar' value="+id+"> Cancelar </button> ");
-
+		var pai =$(this).parent();
+        avo =pai.parent();
+		avo.css('height','200');
+		$(this).after("<div class='comentarioMedico'><br/><textarea id='obs' rows='5' style=width:100%;></textarea><br/><button id='enviarObs' value="+id+"> Enviar </button> <button id='cancelar' value="+id+"> Cancelar </button> </div>");
 
 	});
 
 	$(document).on("click", "#cancelar", function(){
 		$('#obs').remove();
 		$('#enviarObs').remove();
-		$('#cancelar').remove();
+
 	});	
 
 	$(document).on("click","#enviarObs", function(event) {
-				
+					
 
 		$.post(url+'solicitacao/salvarComentarioConsulta', {observacao:$('#obs').val(),solicitacao_id:$(this).val()}, function(data) {
 				
@@ -33,6 +35,8 @@
 				$('#enviarObservacao').remove();
 				$('#enviarObs').remove();
 				$('#cancelar').remove();
+			    $('#cancelar').remove();
+			    avo.remove();
 
 		});
 
