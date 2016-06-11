e667f561aadbd9c1f72e5b02473bc77b840d9ed3
Diogo Vieira Santos <Diogo Vieira Santos>
2016-06-11 15:18:25 -0300

    Refatoração controller instituicao
    Criado metodo para pegar o id da instituição logada em um metodo separado

--- application/controllers/instituicao.php
+++ application/controllers/instituicao.php
@@ -5,28 +5,42 @@ ini_set('display_errors', 1);
 
 class Instituicao extends CI_Controller {
 
+    //Metodo construtor da classe, carrega helper e valida sessão instituição
     function __construct() {
 
 		parent::__construct();
 		$this->load->helper('url');
         $this->login->valida_sessao('instituicao');         
+
 	}
 
+    //Pagina principal do usuario instituição
     public function index(){
 
-        $data = $this->getSolicitacoesInstituicao();
+        $id = $this->getIdInstituicao();
+        $data = $this->getSolicitacoesInstituicao($id);
         $this->load->view('layout/header');
         $this->load->view('solicitacoes_instituicao',$data);
         $this->load->view('layout/footer');
 
     }
 
-    public function getSolicitacoesInstituicao()
-    {
-        $id = $this->session->userdata('instituicao');
+    //Consulta as solicitações efetuadas pelas instituições
+    public function getSolicitacoesInstituicao($id){
+
         $this->load->model('solicitacaoModel', 'sm');
-        $data['query'] = $this->sm->verSolicitcoesInstituicao($id['id_instituicao']);
+        $data['query'] = $this->sm->verSolicitcoesInstituicao($id);
         return $data;
+
+    }
+
+    //Pega o ID da instituicao logada
+    public function getIdInstituicao(){
+
+        $dadosSessao = $this->session->userdata('instituicao');
+        $id = $dadosSessao['id_instituicao'];
+        return $id;
+
     }
 
 }
